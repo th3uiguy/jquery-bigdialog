58791195ef7c8affdbc251ac9d3c0a717db554d7
th3uiguy <th3uiguy@server>
2012-09-03 11:15:09 -0600

    Version 1.5
    - Updated read me file to reflect the changes to simplify the options.
    - The code is updated to use the jQuery UI plugin framework.
    - The version of jQuery UI which the demo was using was buggy which the
    dialog buttons, updated to 1.8.23

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ Git: [https://github.com/th3uiguy/jquery-bigdialog.git](https://github.com/th3ui
 
 Description
 ---------------------
-jQuery Big Dialog scales to fit the full size of the screen with widow resize handling built in. It uses the jQuery UI Dialog options plus a few extra.
+jQuery Big Dialog scales to fit the full size of the screen with widow resize handling built in. It uses 
+the [jQuery UI Dialog options](http://jqueryui.com/demos/dialog/#options) plus a few extra.
 
 
 
@@ -34,19 +35,30 @@ Options
 ---------------------
 Below is a list of the options (in addition to the [jQuery UI Dialog options](http://jqueryui.com/demos/dialog/#options)) that can be passed into `bigDialog` with their default settings.
 
-```js
-	verticalPadding: 80, //Integer, padding on top and bottom of dialog
-	horizontalPadding: 80, //Integer, padding on left and right (40 + 40 = 80 total)
-	scrollLock: true, //Boolean, stop parent window from scrolling when dialog is open
-	scaleIframe: true, //Boolean, if the dialog has an iframe as a child it will the iframe
-	verticalOffset: 55 //Integer, amount to subtract to compensate for the dialogs title and button bars (when dialog has an iframe)
-```
+#### verticalMargin ####
+*type: Integer*   
+*default: 40*   
+Padding on top and bottom of dialog.
+	
+#### horizontalMargin ####
+*type: Integer*   
+*default: 40*   
+Padding on left and right.
 
+#### scrollLock ####
+*type: Boolean*   
+*default: true*   
+Stop parent window from scrolling when dialog is open.
+	
+#### scaleIframe ####
+*type: Boolean*   
+*default: true*   
+If the dialog has an iframe as a child, it will resize the iframe when the dialog is resized.
 
 
 
 <br /><br />
-Copyright 2012, Spencer Neese   
+Copyright 2012 (c), Spencer Neese [https://github.com/th3uiguy/](https://github.com/th3uiguy/)   
 Dual licensed under the 
 [MIT](https://raw.github.com/th3uiguy/jquery-bigdialog/master/MIT-LICENSE.txt) or 
 [GPL](https://raw.github.com/th3uiguy/jquery-bigdialog/master/GPL-LICENSE.txt) Version 2 licenses.   
--- jquery.bigDialog.js
+++ jquery.bigDialog.js
@@ -1,58 +1,73 @@
 /**
-* BigDialog
+* Big Dialog
 *
 * @fileoverview Dialog scales to fit the full size of the screen with widow resize handling built in
 * @link https://github.com/th3uiguy/jquery-bigdialog
 * @author Spencer Neese
-* @version 1.0
+* @version 1.5
 * @requires jQuery UI 1.7+ and jQuery 1.3.2+
-*
-* @license jQuery BigDialog Plugin v1.0
-* https://github.com/th3uiguy/jquery-bigdialog
+* @license jQuery Big Dialog Plugin
 *
 * Copyright 2011, Spencer Neese
 * Dual licensed under the MIT or GPL Version 2 licenses.
-* <https://github.com/th3uiguy/jquery-bigdialog/master/GPL-LICENSE.txt https://github.com/th3uiguy/jquery-bigdialog/master/MIT-LICENSE.txt>
+* <https://raw.github.com/th3uiguy/jquery-bigdialog/master/GPL-LICENSE.txt> <https://raw.github.com/th3uiguy/jquery-bigdialog/master/MIT-LICENSE.txt>
 */
 
 ;(function($) {
-	$.fn.bigDialog = function(options) {
-		// default options
-		var defaults = {
-			verticalPadding: 80,
-			horizontalPadding: 80,
-			scrollLock: true, //stop parent window from scrolling when dialog is open
-			scaleIframe: true,
-			verticalOffset: 55 //amount to subtract to compensate for the dialogs title and button bars (when dialog has an iframe)
-		};
-		var opts = $.extend(true, {}, defaults, options);
+	$.widget( "ui.bigDialog", {
+		options: {
+			verticalMargin: 40,
+			horizontalMargin: 40,
+			scrollLock: true, 
+			scaleIframe: true
+		},
 
-		return this.each(function() {
-			var $self = $(this);
-		
+		_create: function(){
+			var opts = this.options;
+			var $self = $(this.element), vOffset;
+			var vMargin = isFinite(opts.verticalMargin)? opts.verticalMargin*2 : 0;
+			var hMargin = isFinite(opts.horizontalMargin)? opts.horizontalMargin*2 : 0;
 			var iframe = $self.children('iframe');
+
 			if(iframe.size() > 0){
 				opts.height = "auto";
-				if(opts.scaleIframe == true) iframe.css('width', '100%');
-				iframe.css('height', ($(window).height() - opts.verticalPadding - opts.verticalOffset) + 'px');
-			}
-			else{
-				opts.height = $(window).height() - opts.verticalPadding;
-			}
-			opts.width = $(window).width() - opts.horizontalPadding;
-			
-		
-			$(window).resize(function(){
-				$self.dialog('option', 'width', $(window).width() - opts.horizontalPadding);
-				if(iframe.size() > 0){
-					iframe.css('height', ($(window).height() - opts.verticalPadding - opts.verticalOffset) + 'px');
+				if(opts.scaleIframe === true){
+					iframe.css('width', '100%');
+					opts.width = $(window).width() - hMargin;
 				}
 				else{
-					$self.dialog('option', 'height', $(window).height() - opts.verticalPadding);
+					opts.width = "auto";
+				}
+			}
+			else{
+				opts.height = $(window).height() - vMargin;
+				opts.width = $(window).width() - hMargin;
+			}
+
+			$(window).resize(function(){
+				$self.dialog('option', 'width', $(window).width() - hMargin);
+				if(iframe.size() > 0){
+					iframe.css('height', ($(window).height() - vMargin - vOffset) + 'px');
+				}
+				else{
+					$self.dialog('option', 'height', $(window).height() - vMargin);
+				}
+			});
+
+			$self.bind("dialogopen.setHeight", function(){
+				if(iframe.size() > 0 && opts.scaleIframe === true){
+					vOffset = 0;
+					vOffset += $self.siblings('.ui-dialog-titlebar').outerHeight();
+					vOffset += $self.siblings('.ui-dialog-buttonpane').outerHeight();
+					vOffset += parseInt($self.css('padding-top')) + parseInt($self.css('padding-bottom'));
+					vOffset += parseInt($self.closest('.ui-dialog').css('padding-top')) + parseInt($self.closest('.ui-dialog').css('padding-bottom'));
+					vOffset += parseInt($self.siblings('.ui-dialog-buttonpane').css('margin-top'));
+					iframe.css('height', ($(window).height() - vMargin - vOffset) + 'px');
+					$self.dialog("option", "position", "center");
 				}
 			});
 			
-			if(opts.scrollLock == true){
+			if(opts.scrollLock === true){
 				$self.bind("dialogopen.lockscroll", function(){
 					$(window.document.body).css('overflow', 'hidden').find('.ui-widget-overlay').css('width', '100%');;
 				});
@@ -62,7 +77,10 @@
 			}
 			
 			$self.dialog(opts);
-		});
-	};
+		},
 
+		destroy: function() {
+			$.Widget.prototype.destroy.call( this );
+		}
+	});
 })(jQuery);
